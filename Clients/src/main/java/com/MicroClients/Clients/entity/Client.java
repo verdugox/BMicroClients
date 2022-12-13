9c4c50dd99455f250c4732f27789d20f2c1053d3
verdugox <luisandresverdugox@gmail.com>
2022-12-13 00:15:05 -0500

    Subiendo cambios de eureka en BMicroClient

--- Clients/src/main/java/com/MicroClients/Clients/entity/Client.java
+++ Clients/src/main/java/com/MicroClients/Clients/entity/Client.java
@@ -6,7 +6,6 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-
 import javax.persistence.Column;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
